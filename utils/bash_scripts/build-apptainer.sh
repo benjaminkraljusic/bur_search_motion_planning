6089b64f952b9fdde451f5f2ee0e86f0e11f508a
benjaminkraljusic <benjamin.kraljusic@gmail.com>
2025-02-05 17:54:57 +0100

    Added more submodules.

---
+++ utils/bash_scripts/build-apptainer.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+user_specified_path="$1"
+container_path="${user_specified_path:-"${HOME}"/bur_search_apptainer}"
+ws_path="$(dirname "$(realpath "$0")")"/../../../..
+
+if [[ ! -f "${container_path}"/singularity ]]; then
+    apptainer build \
+        --bind "${ws_path}":/opt/catkin_ws \
+        --sandbox "${container_path}" \
+        "${ws_path}"/src/bur_search_motion_planning/utils/apptainer/bur_search_planning.def
+else
+    apptainer exec \
+        --writable "${container_path}" \
+        "${ws_path}"/src/bur_search_motion_planning/utils/bash_scripts/build-ws.sh
+fi
