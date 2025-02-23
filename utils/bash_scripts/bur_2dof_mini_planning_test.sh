509b37c6696fcb91c2a582fd7467c92470c5ba5b
benjaminkraljusic <benjamin.kraljusic@gmail.com>
2025-02-23 16:53:02 +0100

    Added distanceToCollision method to CollisionSpaceMultithread with  getCollisionDistance in SelfCollisionModel. Added planar_2dof_mini robot for debugging.

--- utils/bash_scripts/bur_2dof_mini_planning_test.sh
+++ utils/bash_scripts/bur_2dof_mini_planning_test.sh
@@ -13,7 +13,7 @@ planner="arastar"
 problem_number=1
 
 dataset="$(rospack find smpl_test)/2DoF_planar/planar_2dof_datasets"
-roslaunch --wait smpl_test goal_planar_2dof_bur_planning.launch \
+roslaunch --wait smpl_test goal_planar_2dof_mini_bur_planning.launch \
     problem_index:="${problem_number}" \
     dataset:="${dataset}" \
     planner:="${planner}" \
