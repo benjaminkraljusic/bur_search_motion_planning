4f27ba2c406924e4944de7337f58116c6dd220e6
benjaminkraljusic <benjamin.kraljusic@gmail.com>
2025-04-13 11:19:51 +0200

    Fixed package.xml for bur_search_utils. Removed unnecessary files.

--- bur_search_utils/bash_scripts/build-apptainer.sh
+++ bur_search_utils/bash_scripts/build-apptainer.sh
@@ -8,9 +8,9 @@ if [[ ! -f "${container_path}"/singularity ]]; then
     apptainer build \
         --bind "${ws_path}":/opt/catkin_ws \
         --sandbox "${container_path}" \
-        "${ws_path}"/src/bur_search_motion_planning/utils/apptainer/bur_search_planning.def
+        "${ws_path}"/src/bur_search_motion_planning/bur_search_utils/apptainer/bur_search_planning.def
 else
     apptainer exec \
         --writable "${container_path}" \
-        "${ws_path}"/src/bur_search_motion_planning/utils/bash_scripts/build-ws.sh
+        "${ws_path}"/src/bur_search_motion_planning/bur_search_utils/bash_scripts/build-ws.sh
 fi
